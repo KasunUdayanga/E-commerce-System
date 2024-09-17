c6952f31c286a30fc75ee46a0349f38dded01c4a
Kasun_Udayanga <udayangakasun696@gmail.com>
2024-09-17 16:54:27 +0530

    add

--- laravel-ecommerce-system/platform/plugins/audit-log/public/js/audit-log.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-/*!*********************************************************************!*\
-  !*** ./platform/plugins/audit-log/resources/assets/js/audit-log.js ***!
-  \*********************************************************************/
-$(document).ready(function () {
-  BDashboard.loadWidget($('#widget_audit_logs').find('.widget-content'), route('audit-log.widget.activities'));
-});
-/******/ })()
-;
--- laravel-ecommerce-system/platform/plugins/blog/public/js/blog.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-/*!***********************************************************!*\
-  !*** ./platform/plugins/blog/resources/assets/js/blog.js ***!
-  \***********************************************************/
-$(document).ready(function () {
-  BDashboard.loadWidget($('#widget_posts_recent').find('.widget-content'), route('posts.widget.recent-posts'));
-});
-/******/ })()
-;
