d04c92e6962109d621eea00d84051f73abbd5772
Kasun_Udayanga <udayangakasun696@gmail.com>
2024-09-30 22:09:38 +0530

    add

--- doctor/patient.php
+++ doctor/patient.php
@@ -4,13 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Single Bootstrap CSS Link -->
+
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css" rel="stylesheet">
     
     <link rel="stylesheet" href="../css/animations.css">  
     <link rel="stylesheet" href="../css/main.css">  
-    <!-- <link rel="stylesheet" href="../css/admin.css"> -->
-    <!-- <link rel="stylesheet" href="../css/navbar.css"> -->
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-k6RqeWeci5ZR/Lv4MR0sA0FfDOMdWl3w58Bvv0RmZr1r4R6hh5C3m5eF3R/qJ3" crossorigin="anonymous">
     <title>Patients</title>
     <style>
